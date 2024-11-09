9c697f9a9962a2e1a410f0c9870e3d1ba140ba94
Srisaiarun <2310080060@klh.edu.in>
2024-11-09 23:27:44 +0530

    app60_3/         db.sqlite3         manage.py         project60_3/

---
+++ app60_3/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class App603Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app60_3'
---
+++ app60_3/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+def resume_view(request):
+    # Sample data to be rendered in the template
+    context = {
+        'name': 'Sri Sai Arun',
+        'father_name': 'Srinivas',
+        'mother_name': 'Padmaja',
+        'contact_info': {
+            'email': 'janagamssarun22@gmail.com',
+            'phone': '9502473298',
+            'address': 'Ramanthapur, Hyderabad, Telangana, India'
+        },
+        'education': [ 
+            {'level': 'High School', 'school': 'Cardinal Gracious High School', 'year': '2021'},
+            {'level': 'Intermediary', 'school': 'Narayana Junior College', 'year': '2023'},
+            {'level': 'Graduation', 'school': 'KL University', 'year': '2027'},
+        ],
+        'hobbies': [
+            'Dancing',
+            'Gaming',
+            'Singing',
+            'Photography'
+        ],
+        'achievements': [
+        ],
+        'github_link': 'https://github.com/srisaiarun',  # Add your GitHub link here
+        'photo_url': 'app60_3/arun.jpg',  # Path to the photo (update with your actual file location)
+    }
+    return render(request, 'resume.html', context)
