785b2f347df5bf1c6567a77b4f2dc0562155bf47
Srisaiarun <2310080060@klh.edu.in>
2024-11-10 00:30:38 +0530

    Update settings.py

--- project60_3/settings.py
+++ project60_3/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['resumewebpage.onrender.com', 'your-other-domain.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
