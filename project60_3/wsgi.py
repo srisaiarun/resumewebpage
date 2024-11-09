59c3b151eb656d8ec4a4772cd4040c3d038e59b4
Srisaiarun <2310080060@klh.edu.in>
2024-11-10 00:24:04 +0530

    Update wsgi.py and add Procfile, requirements.txt, and runtime.txt

--- project60_3/wsgi.py
+++ project60_3/wsgi.py
@@ -1,6 +1,6 @@
-import os 
-from django.core.wsgi import get_wsgi_application 
- 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project60_3.settings') 
- 
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project60_3.settings')
+
 application = get_wsgi_application()
